bb08f7262ed293cb50966bea6d67b6aab371df8b
talksik <patel.arjun50@gmai.com>
2023-08-28 13:57:55 -0700

    fixing things

--- device.sh
+++ device.sh
@@ -2,7 +2,14 @@
 # this is special script for running on jetson nano
 
 export QT_DEBUG_PLUGINS=1
+
+export QT_QPA_EGLFS_DEBUG=1
+# the below will run on kms. it will not work on X11/desktop environment
+# export QT_QPA_EGLFS_INTEGRATION=eglfs_kms
+export QT_QPA_EGLFS_HIDECURSOR=1
+
 export GST_DEBUG=2
+export QT_QPA_EGLFS_DEBUG=1
 
 rm -rf build
 mkdir build
